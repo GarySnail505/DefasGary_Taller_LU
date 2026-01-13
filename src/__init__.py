ba48e27346a724f2aa46e32d893ee456893c1bfd
GarySnail505 <gary.defas@hotmail.com>
2026-01-13 17:23:15 -0500

    first commit

---
+++ src/__init__.py
@@ -0,0 +1,2 @@
+from .linear_syst_methods import eliminacion_gaussiana, descomposicion_LU, resolver_LU, matriz_aumentada, separar_m_aumentada, gauss_jordan_inversa  # type: ignore
+from .min_cuadrados import ajustar_min_cuadrados  # type: ignore
